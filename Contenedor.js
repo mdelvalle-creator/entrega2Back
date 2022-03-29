e7dbe6b7957d11dafc3c0493e98605d87e0f832c
mdelvalle-creator <mpia.dv95@gmail.com>
2022-03-29 19:04:27 -0300

    Segunda iteracion

--- Contenedor.js
+++ Contenedor.js
@@ -3,36 +3,53 @@ const fs = require('fs');
 class Contenedor {
     constructor(nombreArchivo) {
       this.nombreArchivo = nombreArchivo;
-      fs.promises.readFile(nombreArchivo, 'utf-8')
-        .then((contenido) => {
-            console.log(contenido);
-            this.fileObject = JSON.parse(contenido)})
+      
+    }
+    async loadFile() {
+        await fs.promises.readFile(this.nombreArchivo, 'utf-8')
+        .then(contenido => {
+            this.fileObject = JSON.parse(contenido)
+        })
         .catch(err => console.log('Error al leer el archivo: ', err));
     }
-    save(objeto) {
+    async save(objeto) {
+        if(!this.fileObject){
+            await this.loadFile();
+        }
         const id = this.fileObject.lastId + 1;
         this.fileObject.list[id] = {...objeto, id};
         this.fileObject.lastId = id;
-        fs.promises.writeFile(this.nombreArchivo, this,JSON.stringify(this.fileObject))
+        await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(this.fileObject))
             .then(() => console.log('Guardado Exitoso!'))
             .catch(err => console.log('Ocurrio un error al guardar: ', err));
-
     }
-    getById(id) {
+    async getById(id) {
+        if(!this.fileObject){
+            await this.loadFile();
+        }
         return this.fileObject.list[id];
     }
-    getAll() {
+    async getAll() {
+        if(!this.fileObject){
+            await this.loadFile();
+        }
         return Object.keys(this.fileObject.list).map(id => this.fileObject.list[id]);
     }
-    deleteById(id) {
+    async deleteById(id) {
+        if(!this.fileObject){
+            await this.loadFile();
+        }
         delete this.fileObject.list[id];
-        fs.promises.writeFile(this.nombreArchivo, this,JSON.stringify(this.fileObject))
+        await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(this.fileObject))
             .then(() => console.log('Eliminado del item exitoso!'))
             .catch(err => console.log('Ocurrio un error al eliminar: ', err));
     }
-    deleteAll() {
+    async deleteAll() {
+        if(!this.fileObject){
+            await this.loadFile();
+        }
         this.fileObject.list = [];
-        fs.promises.writeFile(this.nombreArchivo, this,JSON.stringify(this.fileObject))
+        await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(this.fileObject))
             .then(() => console.log('Eliminado exitoso!'))
             .catch(err => console.log('Ocurrio un error al eliminar: ', err));
     }
--- test.js
+++ test.js
@@ -1,11 +1,48 @@
 const Contenedor = require('./Contenedor')
 
-const container = new Contenedor(`${__dirname}/productos.txt`);
+const testFunction = async () => {
+    const container = new Contenedor(`${__dirname}/productos.txt`);
+    await container.save({
+        title: 'Escuadra',
+        price: 123.45,
+        thumbnail: 'urlToThumbnailEscuadra'
+    });
 
-container.save({
-    title: 'Escuadra',
-    price: 123.45,
-    thumbnail: 'urlToThumbnailEscuadra'
-});
+    let contenido = await container.getAll();
+    console.log('Agrego escuadra ', contenido);
+    
+    await container.save({
+        title: 'Regla',
+        price: 567.89,
+        thumbnail: 'urlToThumbnailRegla'
+    });
 
-console.log('all is good');
+    contenido = await container.getAll();
+    console.log('Agrego regla ', contenido);
+    
+    await container.save({
+        title: 'Semi Circulo',
+        price: 1000,
+        thumbnail: 'urlToThumbnailSemicirculo'
+    });
+
+    contenido = await container.getAll();
+    console.log('Agrego Semicirculo ', contenido);
+
+    contenido = await container.getById(3);
+    console.log('Obtengo Semicirculo ', contenido);
+    
+    await container.deleteById(3);
+
+    contenido = await container.getAll();
+    console.log('Obtengo todos menos Semicirculo ', contenido);
+    
+    await container.deleteAll();
+    
+    contenido = await container.getAll();
+    console.log('Elimino todos ', contenido);
+    
+    console.log('Test completed');
+}
+
+testFunction();
